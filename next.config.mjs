e22a024fb418e1195ac3eac9b15ce5822a2af9a6
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-11-25 00:31:55 -0800

    change next.config again

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+const nextConfig = {
+    basePath: '/slovene-step-by-step'
+}
 
 export default nextConfig;
