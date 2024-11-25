1fb5494588953fb192f3b201d4b01f2070c308a1
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-11-25 00:45:08 -0800

    change the path of the logo

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 // const nextConfig = {};
 const nextConfig = {
-    basePath: '/slovene-step-by-step'
+    basePath: 'https://generalseba.github.io/slovene-step-by-step/'
 }
 
 export default nextConfig;
