40aa55e75137c8cf42de3cc03b39595b61f62e1f
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-11-25 00:29:09 -0800

    reset the next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-const nextConfig = {
-    basePath: '/slovene-step-by-step', // Replace 'repo-name' with your GitHub repository name
-    assetPrefix: '/slovene-step-by-step/',
-  };
+const nextConfig = {};
 
 export default nextConfig;
