5174349d9bbe6eeb9db6820db3c5aa0f9691e650
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-11-25 00:26:50 -0800

    change next.config file so that photos are display at github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+const nextConfig = {
+    basePath: '/slovene-step-by-step', // Replace 'repo-name' with your GitHub repository name
+    assetPrefix: '/slovene-step-by-step/',
+  };
 
 export default nextConfig;
