95b370cda7e11ce503e2e053f7a711b9489f464d
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-12-05 17:37:15 -0800

    edit next.config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: 'https://generalseba.github.io/slovene-step-by-step/',
+};
 
  
 export default withNextIntl(nextConfig);
