b4a16ed3b3b873ddc5773a8c46ee15ac40562cd3
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-12-06 00:29:00 -0800

    edit next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
+import createNextIntlPlugin from "next-intl/plugin";
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+const nextConfig = {
+  output: "export", // Ensures static HTML export
+  trailingSlash: true, // Adds trailing slashes for better GitHub Pages compatibility
+  basePath: "https://generalseba.github.io/slovene-step-by-step/",
+};
 // const nextConfig = {
-//     basePath: 'https://generalseba.github.io/slovene-step-by-step/',
 // };
- 
+
 export default withNextIntl(nextConfig);
