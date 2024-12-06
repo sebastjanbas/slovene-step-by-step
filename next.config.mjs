3a5f36ef9a6102041a89e6d0717ea0c9921aa9b7
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-12-05 16:02:04 -0800

    reset the github config and edit the next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Configure `pageExtensions` to include MDX files
+    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"]
+  };
  
 export default withNextIntl(nextConfig);
