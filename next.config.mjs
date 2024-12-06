526bfa05966af3227ccb8b043b12cf7bcd2fdaf2
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-12-05 21:22:21 -0800

    edit next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Required for static export
     basePath: 'https://generalseba.github.io/slovene-step-by-step/',
-    trailingSlash: true, // Ensure static export works properly
 };
 
  
