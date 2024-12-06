67dd5b9e1843975febfdded97559cf110734c8ba
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-12-06 00:37:00 -0800

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-const nextConfig = {
-  output: "export", // Ensures static HTML export
-//   trailingSlash: true, // Adds trailing slashes for better GitHub Pages compatibility
-};
-// const nextConfig = {
-// };
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
