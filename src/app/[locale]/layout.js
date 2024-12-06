058a099edd02bc5cb20ffbcc783f0757737edb68
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-12-05 23:45:27 -0800

    remove await params

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -21,9 +21,9 @@ export const metadata = {
 //   return routing.locales.map((locale) => ({ locale }));
 // }
 
-export default async function Layout({ children, params}) {
+export default async function LocaleLayout({ children, params: {locale}}) {
 
-  const { locale } = await params;
+  // const { locale } = await params;
 
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale)) {
