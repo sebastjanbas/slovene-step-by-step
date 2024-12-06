0b056e7ea91b76f3cdda012d95daa81cc4d2386b
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2024-12-06 00:49:52 -0800

    resent next config update npm

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-// import createNextIntlPlugin from "next-intl/plugin";
-
-// const withNextIntl = createNextIntlPlugin();
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default withNextIntl(nextConfig);
-
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
