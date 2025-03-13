03ffd2c000f9d7d5cf54ca6dce1a2f4f66dd8f6f
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2025-03-13 16:19:48 -0700

    edit admin panel, add upload videos functionallity

--- src/app/[locale]/(protected)/(admin)/management/page.jsx
+++ src/app/[locale]/(protected)/(admin)/management/page.jsx
@@ -1,10 +1,78 @@
+import { AreaChart1 } from "@/components/dashboard/area-chart";
+import FileInput from "@/components/dashboard/file-input";
+import { RadialChart1 } from "@/components/dashboard/radial-chart";
 import React from "react";
 
 const ManagementPage = () => {
+  const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ];
+
+  const secondChart = [
+    { month: "January", desktop: 83, mobile: 429 },
+    { month: "February", desktop: 291, mobile: 120 },
+    { month: "March", desktop: 512, mobile: 820 },
+    { month: "April", desktop: 175, mobile: 65 },
+    { month: "May", desktop: 23, mobile: 400 },
+    { month: "June", desktop: 782, mobile: 999 },
+  ];
+
+  const value = 69;
+  const total = 100;
+
+  const radialData = {
+    value: value,
+    total: total,
+    list: [
+      {
+        desc: `out of ${total.toLocaleString()}`,
+        visitors: value,
+        fill: "var(--color-safari)",
+      },
+    ],
+  };
+
   return (
-    <>
+    <div className="w-full h-[90vh]">
       <h1 className="w-full text-center text-5xl ">Management</h1>
-    </>
+      <div className="w-full h-full flex flex-col justify-start p-10 items-center">
+        <div className="w-full h-fit flex flex-col  md:flex-row justify-between items-center">
+          <div className="w-full">
+            <AreaChart1
+              title={"My chart"}
+              description={"description"}
+              data={chartData}
+            />
+          </div>
+          <div className="w-full">
+            <RadialChart1
+              title={"My chart"}
+              description="description"
+              data={(radialData.value / radialData.total) * 100}
+              info={radialData.list}
+            />
+          </div>
+          <div className="w-full">
+            <AreaChart1
+              title={"My chart"}
+              description={"description"}
+              data={secondChart}
+            />
+          </div>
+        </div>
+        <div className="mt-10">
+          <h1 className="text-3xl text-center">Add video courses</h1>
+          <div className="p-10">
+            <FileInput />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/dashboard/area-chart.jsx
@@ -0,0 +1,93 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+// const chartData = [
+//   { month: "January", desktop: 186, mobile: 80 },
+//   { month: "February", desktop: 305, mobile: 200 },
+//   { month: "March", desktop: 237, mobile: 120 },
+//   { month: "April", desktop: 73, mobile: 190 },
+//   { month: "May", desktop: 209, mobile: 130 },
+//   { month: "June", desktop: 214, mobile: 140 },
+// ]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "var(--chart-3)",
+  },
+} 
+
+export function AreaChart1({title, description, data}) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={data}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid className="stroke-black/20 dark:stroke-white/20" strokeDasharray={"10 10"} vertical={true} horizontal={true}  />
+            <XAxis
+              dataKey="month"
+              tickLine={true}
+              axisLine={true}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <YAxis domain={[0, 1000]} />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <Line
+              dataKey="desktop"
+              type="monotone"
+              stroke="var(--color-desktop)"
+              strokeWidth={2}
+              dot={false}
+            />
+            <Line
+              dataKey="mobile"
+              type="monotone"
+              stroke="var(--color-mobile)"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter>
+        <div className="italic">
+          Footer
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
+
