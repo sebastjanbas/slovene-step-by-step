3ce784008b6ebfe7d14a85dfe0015cc615527c37
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2025-07-29 14:12:15 +0200

    fix the safari items alignment bug

--- src/components/content/hero-section.tsx
+++ src/components/content/hero-section.tsx
@@ -99,53 +99,56 @@ export default function Hero() {
           />
         </div>
       </div>
-      <div className="relative h-screen sm:h-fit flex flex-row-reverse mx-8 gap-x-5 lg:gap-x-20 lg:max-w-7xl justify-self-center">
-        <div className="hidden sm:block relative aspect-square sm:h-48 sm:mt-60 md:h-56 lg:h-96 lg:mt-40">
-          <Image
-            height={400}
-            width={400}
-            className="aspect-square w-auto sm:h-48 md:h-56 lg:h-96 object-cover rounded-full bg-accent"
-            src="/herro-picture.png"
-            alt=""
-          />
-        </div>
-        <div className="w-full py-[calc(100vh-90vh)] sm:py-48 lg:py-56">
-          <div className="mb-8 flex justify-center">
-            <div className="hidden">
-              {t("announcement")}{" "}
-              <Link href={"#"} className="font-semibold text-">
-                <span aria-hidden="true" className="absolute inset-0" />
-                {t("announcement-link")} <span aria-hidden="true">&rarr;</span>
-              </Link>
-            </div>
+      <div className="flex justify-center items-center">
+        <div className="relative h-screen sm:h-fit flex flex-row-reverse mx-8 gap-x-5 lg:gap-x-20 lg:max-w-7xl">
+          <div className="hidden sm:block relative aspect-square sm:h-48 sm:mt-60 md:h-56 lg:h-96 lg:mt-40">
+            <Image
+              height={400}
+              width={400}
+              className="aspect-square w-auto sm:h-48 md:h-56 lg:h-96 object-cover rounded-full bg-accent"
+              src="/herro-picture.png"
+              alt=""
+            />
           </div>
-          <div className="text-center sm:text-start">
-            <h1
-              ref={titleRef}
-              className="text-balance text-5xl font-semibold tracking-tight text-sl-primary md:text-6xl lg:text-7xl"
-            >
-              <div>
-                {t("title-1")}{" "}
-                <strong className="text-accent">{t("title-strong")}</strong>
-                {t("title-2")}
+          <div className="w-full py-[calc(100vh-90vh)] sm:py-48 lg:py-56">
+            <div className="mb-8 flex justify-center">
+              <div className="hidden">
+                {t("announcement")}{" "}
+                <Link href={"#"} className="font-semibold text-">
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  {t("announcement-link")}{" "}
+                  <span aria-hidden="true">&rarr;</span>
+                </Link>
               </div>
-            </h1>
-            <p
-              ref={subtitleRef}
-              className="mt-8 text-pretty text-lg font-medium text-sl-secondary sm:text-xl/8"
-            >
-              {t("under-title")}
-            </p>
-            <div
-              ref={buttonRef}
-              className="mt-10 flex items-center justify-center sm:justify-start gap-x-6"
-            >
-              <a
-                href={"/dashboard"}
-                className="rounded-2xl bg-sl-accent hover:bg-sl-accent-hover px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm transition-colors duration-200 ease-in-out"
+            </div>
+            <div className="text-center sm:text-start">
+              <h1
+                ref={titleRef}
+                className="text-balance text-5xl font-semibold tracking-tight text-sl-primary md:text-6xl lg:text-7xl"
               >
-                {t("action-button")}
-              </a>
+                <div>
+                  {t("title-1")}{" "}
+                  <strong className="text-accent">{t("title-strong")}</strong>
+                  {t("title-2")}
+                </div>
+              </h1>
+              <p
+                ref={subtitleRef}
+                className="mt-8 text-pretty text-lg font-medium text-sl-secondary sm:text-xl/8"
+              >
+                {t("under-title")}
+              </p>
+              <div
+                ref={buttonRef}
+                className="mt-10 flex items-center justify-center sm:justify-start gap-x-6"
+              >
+                <a
+                  href={"/dashboard"}
+                  className="rounded-2xl bg-sl-accent hover:bg-sl-accent-hover px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm transition-colors duration-200 ease-in-out"
+                >
+                  {t("action-button")}
+                </a>
+              </div>
             </div>
           </div>
         </div>
