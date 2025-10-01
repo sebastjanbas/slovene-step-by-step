24ccfd6c5fc2b5f06ba223a9b5c5be534c4d5e9b
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2025-10-01 14:39:30 -0700

    update middleware to open access to sitemap.xml

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,18 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
-import { clerkMiddleware } from '@clerk/nextjs/server';
- 
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
 // export async function middleware(request) {
 //   return await updateSession(request)
 // }
 
 // export default createMiddleware(routing);
- 
 
 // export const config = {
 //   // Match only internationalized pathnames
 //   // matcher: ['/', '/(sl|ru|en)/:path*']
 //   matcher: [
-//     '/', 
+//     '/',
 //     '/(sl|ru|en)/:path*',
 //     '/((?!_next/static|_next/image|favicon.ico|public).*)'
 //   ]
@@ -21,7 +20,7 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware(async (auth, req) => {
   // Skip i18n middleware for API routes
-  if (req.nextUrl.pathname.startsWith('/api/')) {
+  if (req.nextUrl.pathname.startsWith("/api/")) {
     return;
   }
 
@@ -29,18 +28,16 @@ export default clerkMiddleware(async (auth, req) => {
   const response = await i18nMiddleware(req);
 
   return response;
-})
-
-
+});
 
 export const config = {
   matcher: [
     // Match root or locale-prefixed paths only
-    '/',
-    '/(sl|ru|en|it)/:path*',
-    '/api/:path*',
+    "/",
+    "/(sl|ru|en|it)/:path*",
+    "/api/:path*",
 
     // Match all other paths EXCEPT /sign-in and _next, assets etc.
-    '/((?!sign-in|_next/static|_next/image|images|assets|favicon.ico|auth/confirm|auth/callback|auth/update-password|.*\\.(?:svg|jpg|jpeg|png|gif|ico)$).*)',
-  ]
-}
+    "/((?!sign-in|_next/static|_next/image|images|assets|favicon.ico|sitemap.xml|auth/confirm|auth/callback|auth/update-password|.*\\.(?:svg|jpg|jpeg|png|gif|ico|xml)$).*)",
+  ],
+};
