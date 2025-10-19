72896f55c86078ac2c197ab09790265546b84f8e
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2025-10-19 08:30:12 -0700

    fix routing error

--- src/app/[locale]/(protected)/calendar/page.tsx
+++ src/app/[locale]/(protected)/calendar/page.tsx
@@ -1,7 +1,12 @@
+import Calendar from "@/components/calendar/calendar";
 import React from "react";
 
 const CalendarPage = () => {
-  return <div>CalendarPage</div>;
+  return (
+    <div className="p-5 h-[90vh] md:h-[calc(100vh-80px)] w-full overflow-hidden">
+      <Calendar />
+    </div>
+  );
 };
 
 export default CalendarPage;
